63b96e7af82d96fb23607fdccdcf618c317d1e8c
Kareena Yadav <aarohiyadav44001@gmail.com>
2026-09-11 20:18:09 +0530

    Time: 17 ms (11.13%), Space: 46.4 MB (22.4%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        String cleaned = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+         int n = cleaned.length();
+        for(int i=0; i < n/2; i++){
+           if(cleaned.charAt(i) != cleaned.charAt(n-1-i)){
+            return false;
+           } 
+        }
+        return true;
+    }
+}
