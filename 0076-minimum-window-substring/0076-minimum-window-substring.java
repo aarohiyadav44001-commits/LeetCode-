45e745913366066d2f5278af0fdcaaef6f1ea8b3
Kareena Yadav <aarohiyadav44001@gmail.com>
2026-09-14 21:56:36 +0530

    Time: 17 ms (49.31%), Space: 47.2 MB (45.9%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -0,0 +1,47 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        int n = s.length();
+        if (t.length() > n) return "";
+        
+        Map<Character, Integer> map = new HashMap<>();
+        for (char ch : t.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+        
+        int requiredCount = t.length();
+        int i = 0, j = 0;
+        int minWindowSize = Integer.MAX_VALUE;
+        int start_i = 0;
+        
+        while (j < n) {
+            char ch = s.charAt(j);
+            if (map.containsKey(ch)) {
+                if (map.get(ch) > 0) {
+                    requiredCount--;
+                }
+                map.put(ch, map.get(ch) - 1);
+            }
+            
+            while (requiredCount == 0) {
+                int currWindowSize = j - i + 1;
+                if (minWindowSize > currWindowSize) {
+                    minWindowSize = currWindowSize;
+                    start_i = i;
+                }
+                
+                char leftChar = s.charAt(i);
+                if (map.containsKey(leftChar)) {
+                    map.put(leftChar, map.get(leftChar) + 1);
+                    if (map.get(leftChar) > 0) {
+                        requiredCount++;
+                    }
+                }
+                i++;
+            }
+            j++;
+        }
+        
+        return minWindowSize == Integer.MAX_VALUE ? "" : s.substring(start_i, start_i + minWindowSize);
+    }
+}
+
